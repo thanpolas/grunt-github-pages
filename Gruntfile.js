126002ccd9b47f5e8dffc75ab487651dc73ec5a1
Thanasis Polychronakis <thanpolas@gmail.com>
2013-11-25 15:53:33 +0200

    ocd

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+/*jshint camelcase:false */
+
 /*
  * grunt-github-pages
  * https://github.com/thanpolas/grunt-github-pages
@@ -51,7 +53,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'github_pages', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'githubPages', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
